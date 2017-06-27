d6bc138ea9db5a6ebac3a05ace0b5899cc573864
BrightonNgema <brightonnkosana@gmail.com>
2017-06-27 17:06:06 +0200

    Shopping List

---
+++ client/shoppingList.js
@@ -0,0 +1,11 @@
+Template.ShoppingList.onCreated(function(){
+    var self = this;
+    self.autorun(function(){
+      self.subscribe('recipes');
+    });
+});
+Template.ShoppingList.helpers({
+  recipes:()=> {
+    return Recipes.find({inMenu: true});
+  }
+});
